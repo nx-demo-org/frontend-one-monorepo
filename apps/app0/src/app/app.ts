abfbbe1d0abb0a4fc16e5aa796213b17f36c5555
Josh VanAllen <5290334+joshvanallen@users.noreply.github.com>
2025-07-31 10:41:30 -0400

    adding deps

--- apps/app0/src/app/app.ts
+++ apps/app0/src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
-
+import { examplePublishableLibrary } from '@nx-demo-org/example-javascript-library';
+import { Feature0 } from '@nx-demo-org/feature0';
+import { Feature1} from '@nx-demo-org/feature1';
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, Feature0, Feature1],
   selector: 'app-root',
-  template: `<app-nx-welcome></app-nx-welcome> <router-outlet></router-outlet>`,
+  template: `<app-nx-welcome></app-nx-welcome> <lib-feature0></lib-feature0><lib-feature1></lib-feature1> <router-outlet></router-outlet>`,
   styles: ``,
 })
 export class App {
-  protected title = 'app0';
+  protected title = examplePublishableLibrary();
 }
--- libs/feature0/src/lib/feature0/feature0.ts
+++ libs/feature0/src/lib/feature0/feature0.ts
@@ -1,11 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { examplePublishableLibrary } from '@nx-demo-org/example-javascript-library';
+import {Subfeature0 } from '@nx-demo-org/subfeature0';
+import {Subfeature1 } from '@nx-demo-org/subfeature1';
 
+console.log(examplePublishableLibrary());
 @Component({
   selector: 'lib-feature0',
-  imports: [CommonModule],
-  template: `<p>Feature0 works!</p>`,
+  imports: [CommonModule, Subfeature0, Subfeature1],
+  template: `<p>Feature0 works!</p><lib-subfeature0></lib-subfeature0><lib-subfeature1></lib-subfeature1>`,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Feature0 {}
+export class Feature0 {
+}
